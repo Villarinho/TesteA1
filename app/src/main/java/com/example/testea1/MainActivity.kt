fc1820642b25bcc9f0b312af40939fb6adfc795d
Villarinho <39889946+Villarinho@users.noreply.github.com>
2019-10-31 17:47:25 -0300

    31/10 17:46

--- app/src/main/java/com/example/testea1/MainActivity.kt
+++ app/src/main/java/com/example/testea1/MainActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
@@ -14,17 +13,17 @@ import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Inicialização
         listarPesquisas()
 
         setSupportActionBar(toolbar)
-
-
-//Mensagem aquando clica no botão ao pé da página
+        //Mensagem aquando clica no botão ao pé da página
         fab.setOnClickListener { view ->
             Snackbar.make(view, getString(R.string.aviso_snackbar), Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
@@ -47,13 +46,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    override fun onClick(v: View?) {
-        val intent = Intent(this, Main2Activity::class.java).apply {
-            putExtra("EXTRA_MESSAGE", "teste")
-        }
-        startActivity(intent)
-    }
-
     fun listarPesquisas() {
         var linearLayoutVertical: LinearLayout = findViewById(R.id.layoutHolder)
         val params = LinearLayout.LayoutParams(
@@ -63,29 +55,31 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         //Array for holding refrences
         val buttonHolder: Array<Button?>
 
-        buttonHolder = arrayOfNulls<Button>(20)//Setting size of Button Array
         Pesquisa.downloadListaPesquisa("FRANCISCOCV")// TODO("Pegar usuário real")
+        //      buttonHolder = arrayOfNulls(Pesquisa.getSize())//Setting size of Button Array
 
         //Loop for items
-        for (i in 0..19) {
+        for (i in Pesquisa.lista) {
             val button = Button(this)//Creating Button
-            button.setId(i)//Setting Id for using in future
-            button.setText(Pesquisa.lista.get(i).name)//Setting text
+            button.setId(i.id)//Setting Id for using in future
+            button.setText(i.name)//Setting text
             button.setPadding(5, 5, 5, 5)//paading
             button.setLayoutParams(params)//Setting Layout Params
             button.setTextColor(parseColor("#000000"))//Text Colort
             button.setGravity(Gravity.CENTER)//Gravity of Text
+
             button.setOnClickListener { view ->
                 val intent = Intent(this, Main2Activity::class.java).apply {
                     putExtra(
                         "EXTRA_MESSAGE",
-                        if (view is Button) Pesquisa.lista.get(i).name else "nâo"
+                        //TODO Aqui eu não consegui passar pelas propriedades do button
+                        if (view is Button) i.name else "nâo"
                     )
                 }
                 startActivity(intent)
             }
 
-            buttonHolder[i] = button//Setting button reference in array for future use
+            //          buttonHolder[i] = button//Setting button reference in array for future use
             layoutHolder.orientation = LinearLayout.VERTICAL//Setting Layout orientation
             layoutHolder.addView(button)//Finally adding view
 
--- app/src/main/java/com/example/testea1/Pesquisa.kt
+++ app/src/main/java/com/example/testea1/Pesquisa.kt
@@ -1,14 +1,17 @@
 package com.example.testea1
 
-data class Pesquisa(val name: String, val texto_busca: String) {
+data class Pesquisa(val id: Int, val name: String, val texto_busca: String) {
     companion object {
         val lista = mutableListOf<Pesquisa>()
 
-
-        fun add(name: String, texto_busca: String): Pesquisa {
-            val pesquisa = Pesquisa(name, texto_busca)
+        fun add(id: Int, name: String, texto_busca: String): Pesquisa {
+            val pesquisa = Pesquisa(id, name, texto_busca)
             lista.add(pesquisa)
             return pesquisa
+        }
+
+        fun getSize(): Int {
+            return lista.size
         }
 
         /**
@@ -16,15 +19,15 @@ data class Pesquisa(val name: String, val texto_busca: String) {
          * TODO Acesso ao web service lista de pesquisas
          */
         fun downloadListaPesquisa(usuario: String) {
-            Pesquisa.add("tipologia 1","lagosta")
-            Pesquisa.add("tipologia 2","camarão")
-            Pesquisa.add("tipologia 3","polvo")
-            Pesquisa.add("tipologia 4","siri")
-            for (i:Int  in 5..20 ){
-                Pesquisa.add("tipologia  $i " ,"siri $i")
+            Pesquisa.add(1, "tipologia 1", "lagosta")
+            Pesquisa.add(2, "tipologia 2", "camarão")
+            Pesquisa.add(3, "tipologia 3", "polvo")
+            Pesquisa.add(4, "tipologia 4", "siri")
+            for (i: Int in 5..28) {
+                Pesquisa.add(i, "tipologia  $i ", "siri $i")
             }
-
         }
+
     }
 }
 
