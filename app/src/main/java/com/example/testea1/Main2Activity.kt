529be64414c72398fd44efced2ee957204487f34
Villarinho <39889946+Villarinho@users.noreply.github.com>
2019-10-31 20:40:35 -0300

    31/10 20:39 rating 2 estrelas de 3

--- app/src/main/java/com/example/testea1/Main2Activity.kt
+++ app/src/main/java/com/example/testea1/Main2Activity.kt
@@ -25,25 +25,10 @@ class Main2Activity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val webView: WebView
-        val content = "<h1>Heading 1</h1>\n" +
-                "        <h2>Heading 2</h2>\n" +
-                "        <p>This is some html. Look, here\\'s an <u>underline</u>.</p>\n" +
-                "        <p>Look, this is <em>emphasized.</em> And here\\'s some <b>bold</b>.</p>\n" +
-                "        <p>Here are UL list items:\n" +
-                "        <ul>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ul>\n" +
-                "        <p>Here are OL list items:\n" +
-                "        <ol>\n" +
-                "        <li>One</li>\n" +
-                "        <li>Two</li>\n" +
-                "        <li>Three</li>\n" +
-                "        </ol>"
         webView = findViewById(R.id.webView);
         // displaying text in WebView
-        webView.loadDataWithBaseURL(null, content, "text/html", "utf-8", null);
+        webView.loadUrl("file:///android_asset/sample.html");
+
     }
 
 }
