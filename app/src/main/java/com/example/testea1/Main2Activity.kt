4bf9920f4db8aeaab3569ff4acf6439281ae7870
Villarinho <39889946+Villarinho@users.noreply.github.com>
2019-11-04 19:33:55 -0300

    pronto +

--- app/src/main/java/com/example/testea1/Main2Activity.kt
+++ app/src/main/java/com/example/testea1/Main2Activity.kt
@@ -1,12 +1,11 @@
 package com.example.testea1
 
 import android.os.Bundle
-import android.webkit.WebView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main2.*
-
+import kotlinx.android.synthetic.main.content_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
@@ -15,6 +14,8 @@ class Main2Activity : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
         // Get the Intent that started this activity and extract the string
         val message = this.intent.getStringExtra("EXTRA_MESSAGE")
+        // TODO Processar a EXTRA_MESSAGE que indica qual busca foi selecionada
+        //      Por enquanto coloca o texto na barra de título
         this.title = message
 
         setSupportActionBar(toolbar)
@@ -27,13 +28,14 @@ class Main2Activity : AppCompatActivity() {
 
         //TODO Pega nova anotação do web service e exibe o HTML
         //  Por enquanto pega um arquivo HTML nos assets
-        val webView: WebView = findViewById(R.id.webView);
         webView.loadUrl("file:///android_asset/texto3.html")
 
-        var enviaRotulo: EnviaRotulo = EnviaRotulo(findViewById(R.id.webView))
-        var botaoSim: Button = findViewById(R.id.button4)
-        var botaoDuvida: Button = findViewById(R.id.button6)
-        var botaoNao: Button = findViewById(R.id.button5)
+        var enviaRotulo: EnviaRotulo = EnviaRotulo(webView)
+
+        // Propriedades dos botões
+        var botaoSim: Button = button4
+        var botaoDuvida: Button = button6
+        var botaoNao: Button = button5
         botaoSim.tag = 1
         botaoDuvida.tag = 0
         botaoNao.tag = -1
@@ -41,6 +43,4 @@ class Main2Activity : AppCompatActivity() {
         botaoDuvida.setOnClickListener(enviaRotulo)
         botaoNao.setOnClickListener(enviaRotulo)
     }
-
-
 }
