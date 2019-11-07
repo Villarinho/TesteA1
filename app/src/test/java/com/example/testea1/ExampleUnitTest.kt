093cd25ece0669e9e78bd774629dc36826701805
VIllarinho <villarinho@gmail.com>
2019-11-06 22:07:55 -0300

    adiciona arquivo json para teste

--- app/src/test/java/com/example/testea1/ExampleUnitTest.kt
+++ app/src/test/java/com/example/testea1/ExampleUnitTest.kt
@@ -1,6 +1,13 @@
 package com.example.testea1
 
 import org.junit.Test
+import java.io.IOException
+import java.io.InputStream
+import java.io.File
+import org.json.JSONObject
+
+
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,4 +26,20 @@ class ExampleUnitTest {
 
     }
 
-}
+    fun readFileDirectlyAsText(fileName: String): String
+            = File(fileName).readText(Charsets.UTF_8)
+
+    @Test
+    fun parseJson() {
+   //     val path = System.getProperty("user.dir")+"\\src\\main\\assets\\db.json"
+
+        val json:String = readFileDirectlyAsText("//android_asset/db.json")
+
+     //   val jObject = JSONObject(json)
+    //    val aJsonString = jObject.getString("resposta.html")
+        println(json)
+
+    }
+
+
+ }
